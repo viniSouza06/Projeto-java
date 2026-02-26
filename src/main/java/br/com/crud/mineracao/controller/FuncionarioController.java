563829db9790f2587e42a246c7fc9e3af01b83b5
viniSouza06 <macedovinicius193@gmail.com>
2026-02-26 18:07:52 -0300

    feature front-end

--- src/main/java/br/com/crud/mineracao/controller/FuncionarioController.java
+++ src/main/java/br/com/crud/mineracao/controller/FuncionarioController.java
@@ -3,6 +3,7 @@ package br.com.crud.mineracao.controller;
 import br.com.crud.mineracao.service.FuncionarioService;
 import br.com.crud.mineracao.model.FuncionarioModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
@@ -19,7 +20,7 @@ public class FuncionarioController {
     @PostMapping
     public ResponseEntity<FuncionarioModel> criar(@Valid @RequestBody FuncionarioModel funcionario) {
         FuncionarioModel novoFuncionario = funcionarioService.criar(funcionario);
-        return ResponseEntity.ok(novoFuncionario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoFuncionario);
     }
 
     @GetMapping
@@ -35,18 +36,18 @@ public class FuncionarioController {
     @PutMapping("/{id}")
     public ResponseEntity<FuncionarioModel> atualizar(@PathVariable Long id,
                                                       @Valid @RequestBody FuncionarioModel dadosAtualizados) {
+
         return ResponseEntity.ok(funcionarioService.atualizar(id, dadosAtualizados));
     }
 
     @DeleteMapping("/{idGerente}/{idFuncionario}")
-    public ResponseEntity<Void> remover(@PathVariable Long idGerente,@PathVariable Long idFuncionario) {
+    public ResponseEntity<Void> remover(@PathVariable Long idGerente, @PathVariable Long idFuncionario) {
         funcionarioService.remover(idGerente, idFuncionario);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/cargo/{cargo}")
-    public List<FuncionarioModel> listarPorCargo(@PathVariable String cargo) {
-        return funcionarioService.ListarPorCargo(cargo);
-
+    public ResponseEntity<List<FuncionarioModel>> listarPorCargo(@PathVariable String cargo) {
+        return ResponseEntity.ok(funcionarioService.ListarPorCargo(cargo));
     }
 }
